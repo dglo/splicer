99cdd0b6d00b9613d1612ea68ec397d7adf4c79a
Dave Glowacki <dglo@icecube.wisc.edu>
2007-04-20 11:19:53 -0500

    Issue 1328: Unbreak test

--- src/test/java/icecube/daq/hkn1/HKN1SplicerTest.java
+++ src/test/java/icecube/daq/hkn1/HKN1SplicerTest.java
@@ -10,57 +10,72 @@ import icecube.daq.splicer.HKN1Splicer;
 import icecube.daq.splicer.Spliceable;
 import icecube.daq.splicer.SpliceableFactory;
 import icecube.daq.splicer.SplicedAnalysis;
+import icecube.daq.splicer.SplicerListener;
 import icecube.daq.splicer.Splicer;
+import icecube.daq.splicer.SplicerChangedEvent;
 import icecube.daq.splicer.StrandTail;
 
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Before;
 import org.junit.Test;
 
-public class HKN1SplicerTest implements SplicedAnalysis
+public class HKN1SplicerTest
 {
-
-    SpliceableFactory factory;
-    HKN1Splicer       splicer;
-    boolean           isOrdered = false;
-    int               outputCount;
 
     public HKN1SplicerTest()
     {
         BasicConfigurator.configure();
     }
 
-    @Before
-    public void setUp() throws Exception
-    {
-        isOrdered = true;
-        outputCount = 0;
-    }
-
     @Test
     public void basicUnloadedTest() throws Exception
     {
-        splicer = new HKN1Splicer(this);
+        Analysis analysis = new Analysis();
+        HKN1Splicer splicer = new HKN1Splicer(analysis);
+        analysis.setSplicer(splicer);
         StrandTail tail0 = splicer.beginStrand();
         StrandTail tail1 = splicer.beginStrand();
         splicer.start();
-        for (int i = 0; i < 100; i++)
+        Random r = new Random();
+        final int numObjs = 100;
+        for (int i = 0; i < numObjs; i++)
         {
-            Random r = new Random();
-            double u = r.nextDouble();
-            Thread.sleep((long) (30.0 * u));
-            if (r.nextBoolean())
-                tail1.push(new TimeStamp());
-            else
-                tail0.push(new TimeStamp());
+            TimeStamp obj = new TimeStamp(i + 1);
+            if (r.nextBoolean()) {
+                tail1.push(obj);
+            } else {
+                tail0.push(obj);
+            }
+            //if (i % 30 == 0) Thread.sleep(100);
         }
         tail0.push(Splicer.LAST_POSSIBLE_SPLICEABLE);
         tail1.push(Splicer.LAST_POSSIBLE_SPLICEABLE);
         splicer.stop();
-        Thread.sleep(100);
-        System.out.println("splicer contents = " + splicer.getCount());
-        assertTrue(isOrdered);
-        assertEquals(100, outputCount);
+        for (int i = 0; i < 10 && analysis.getOutputCount() < numObjs; i++) {
+            Thread.sleep(100);
+        }
+        //System.out.println("splicer contents = " + splicer.getCount());
+        assertTrue(analysis.isOrdered());
+        assertEquals(numObjs, analysis.getOutputCount());
+    }
+
+}
+
+class Analysis implements SplicedAnalysis, SplicerListener
+{
+    private boolean unsorted;
+    private int outputCount;
+    private int listOffset;
+    private Splicer splicer;
+    private TimeStamp lastObj;
+
+    Analysis()
+    {
+    }
+
+    public void disposed(SplicerChangedEvent event)
+    {
+        throw new Error("Unimplemented");
     }
 
     /**
@@ -69,11 +84,11 @@ public class HKN1SplicerTest implements SplicedAnalysis
      */
     public void execute(List splicedObjects, int decrement)
     {
-        Iterator it = splicedObjects.iterator();
-        TimeStamp lastObj = null;
-        while (it.hasNext())
+        final int listLen = splicedObjects.size();
+        for (int i = listOffset; i < listLen; i++)
         {
-            TimeStamp obj = (TimeStamp) it.next();
+            TimeStamp obj = (TimeStamp) splicedObjects.get(i);
+
             if (lastObj != null)
             {
                 if (obj.compareTo(lastObj) < 0)
@@ -82,32 +97,63 @@ public class HKN1SplicerTest implements SplicedAnalysis
                             "ERROR: objects exiting splicer are not ordered: ["
                             + lastObj.timestamp + ", " 
                             + obj.timestamp + "].");
-                    isOrdered = false;
+                    unsorted = true;
                 }
             }
             lastObj = obj;
             outputCount++;
         }
-        int n = splicedObjects.size();
-        if (n > 0) splicer.truncate((Spliceable) splicedObjects.get(n - 1));
-        System.out.println("execute called with # items = " + n + " decrement = " + decrement);
+
+        if (listLen > 0)
+        {
+            if (listLen >= listOffset) listOffset = listLen;
+            splicer.truncate((Spliceable) splicedObjects.get(listLen - 1));
+        }
+
+        //System.out.println("execute called with # items = " + listLen + " decrement = " + decrement);
+    }
+
+    public void failed(SplicerChangedEvent event)
+    {
+        throw new Error("Unimplemented");
     }
 
     public SpliceableFactory getFactory()
     {
-        // TODO Auto-generated method stub
-        return factory;
+        return null;
     }
 
+    public int getOutputCount() { return outputCount; }
+    public boolean isOrdered() { return !unsorted; }
+
+    public void setSplicer(Splicer splicer)
+    {
+        this.splicer = splicer;
+        splicer.addSplicerListener(this);
+    }
+
+    public void started(SplicerChangedEvent event) { } 
+    public void starting(SplicerChangedEvent event) { } 
+    public void stopped(SplicerChangedEvent event) { }
+    public void stopping(SplicerChangedEvent event) { }
+
+    public void truncated(SplicerChangedEvent event)
+    {
+        if (event.getSpliceable() == Splicer.LAST_POSSIBLE_SPLICEABLE) {
+            listOffset = 0;
+        } else {
+            listOffset -= event.getAllSpliceables().size();
+        }
+    }
 }
 
 class TimeStamp implements Spliceable
 {
     public long timestamp;
 
-    public TimeStamp()
+    public TimeStamp(long val)
     {
-        timestamp = System.currentTimeMillis();
+        timestamp = val;
     }
 
     public int compareTo(Object o)
@@ -119,4 +165,8 @@ class TimeStamp implements Spliceable
         return 0;
     }
 
+    public String toString()
+    {
+        return Long.toString(timestamp);
+    }
 }
